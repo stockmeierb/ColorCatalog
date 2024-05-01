83e2ebc5bf59c1de672cc962a668c75702c9e947
brettstock <stockmeierb@users.noreply.github.com>
2024-05-01 17:07:29 -0500

    fixed the Alert

--- App.js
+++ App.js
@@ -1,21 +1,19 @@
 import React from "react";
 import { ActivityIndicator, Text, View, Button, Alert } from "react-native";
-// import { ActivityIndicator, ProgressViewIOS, Text, View } from "react-native";
 
 export default function App() {
 const onButtonPress = () => {
   console.log(`${new Date().toLocaleTimeString()} button press`);
-  // Alert.alert(`${new Date().toLocaleTimeString()} button press`);
-};
+  Alert.alert(`${new Date().toLocaleTimeString()} button press`);
   return (
     <View style={{padding: 50}}>
       {/* <ProgressViewIOS progress={0.5} /> */}
       <ActivityIndicator size="large" color={"#61DBFB"} />
-      <Button title="click me"  onButtonPress={onButtonPress} />
+      <Button title="click me"  onPress={onButtonPress} onButtonPress />
     <Text>Hello World</Text>
     <Text>Red</Text>
     <Text>Green</Text>
     <Text>Blue</Text>
     </View>
-  )
+  );
 }
