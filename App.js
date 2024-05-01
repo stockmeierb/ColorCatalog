d82d7a0ec161c9eb5461057a1ddfefaf9c65a5c7
brettstock <stockmeierb@users.noreply.github.com>
2024-05-01 17:16:49 -0500

    add height and width and also fix typo

--- App.js
+++ App.js
@@ -1,19 +1,21 @@
 import React from "react";
-import { ActivityIndicator, Text, View, Button, Alert } from "react-native";
+import { ActivityIndicator, Text, View, Button, Alert, Dimensions } from "react-native";
+
+const { height, width } = Dimensions.get('window');
 
 export default function App() {
 const onButtonPress = () => {
   console.log(`${new Date().toLocaleTimeString()} button press`);
   Alert.alert(`${new Date().toLocaleTimeString()} button press`);
+}
   return (
     <View style={{padding: 50}}>
       {/* <ProgressViewIOS progress={0.5} /> */}
       <ActivityIndicator size="large" color={"#61DBFB"} />
       <Button title="click me"  onPress={onButtonPress} onButtonPress />
     <Text>Hello World</Text>
-    <Text>Red</Text>
-    <Text>Green</Text>
-    <Text>Blue</Text>
+    <Text>Height: {height}</Text>
+    <Text>Width: {width}</Text>
     </View>
   );
 }
