e3a8844ec856b2104d04e9235e920967649e8f06
brettstock <stockmeierb@users.noreply.github.com>
2024-08-09 15:52:07 -0500

    back in the saddle

--- App.js
+++ App.js
@@ -1,8 +1,8 @@
 import React, {useState} from "react";
+import { Text, View, StyleSheet, TouchableHighlight } from "react-native";
 // import { ActivityIndicator, Text, View, Button, Alert, Dimensions, Platform, StyleSheet } from "react-native";
-import { Text, View, StyleSheet } from "react-native";
 
-const { height, width } = Dimensions.get('window');
+// const { height, width } = Dimensions.get('window');
 
 export default function App() {
 // const onButtonPress = () => {
@@ -12,9 +12,22 @@ export default function App() {
 
   const [backgroundColor, setBackgroundColor] = useState("blue");
   return (
-    <View style={[styles.container, { backgroundColor }]} >
-    <Text style={styles.button} onPress={() => setBackgroundColor("green")} >green</Text>
-    <Text style={styles.button} onPress={() => setBackgroundColor("red")} >red</Text>
+    
+    <View style={[styles.container, { backgroundColor }]}>
+      <TouchableHighlight 
+      style={styles.button}
+      onPress={() => setBackgroundColor("yellow")}
+        underlayColor="orange"
+        >
+        
+        <View style={styles.row}>
+          <View style={[styles.sample, { backgroundColor: "yellow" }]} />
+          <Text style={styles.buttonText}>yellow</Text>
+        </View>
+
+      </TouchableHighlight>
+    {/* <Text style={styles.button} onPress={() => setBackgroundColor("green")} >green</Text> */}
+    {/* <Text style={styles.button} onPress={() => setBackgroundColor("red")} >red</Text> */}
     
       {/* <ActivityIndicator size="large" color={"#61DBFB"} /> */}
       {/* <Button title="click me"  onPress={onButtonPress} onButtonPress /> */}
@@ -39,8 +52,25 @@ const styles = StyleSheet.create({
     padding: 10,
     borderWidth: 2,
     borderRadius: 10,
+    alignSelf: "stretch",
     textAlign:"center",
     padding: 5,
+    backgroundColor: "rgba(255,255,255, .8)"
+  },
+  buttonText: {
+    fontSize: 30,
+    textAlign: "center"
+  },
+  row: {
+    flexDirection: "row",
+    alignItems: "center"
+  },
+  sample: {
+    height:20,
+    width:20,
+    margin: 5,
+    borderRadius: 10,
+    backgroundColor: "white"
   },
   selectedText: {
     backgroundColor: "red",
