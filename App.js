6189d08fa21582127e896084b6e386c81b58ff94
brettstock <stockmeierb@users.noreply.github.com>
2024-08-11 16:12:08 -0500

    clean up and organize; create a custom component to import

--- App.js
+++ App.js
@@ -1,25 +1,8 @@
 import React, {useState} from "react";
-import { Text, View, StyleSheet, TouchableHighlight } from "react-native";
-// import { ActivityIndicator, Text, View, Button, Alert, Dimensions, Platform, StyleSheet } from "react-native";
+import { View, StyleSheet } from "react-native";
+import ColorButton from "./components/ColorButton";
 
 // const { height, width } = Dimensions.get('window');
-
-function ColorButton({backgroundColor, onPress = f => f }) {
-  return (
-<TouchableHighlight 
-      style={styles.button}
-      onPress={() => onPress(backgroundColor)}
-        underlayColor="orange"
-        >
-        
-        <View style={styles.row}>
-          <View style={[styles.sample, { backgroundColor: backgroundColor }]} />
-          <Text style={styles.buttonText}>{backgroundColor}</Text>
-        </View>
-
-      </TouchableHighlight>
-  )
-}
 
 export default function App() {
 // const onButtonPress = () => {
@@ -55,35 +38,5 @@ const styles = StyleSheet.create({
     display: "flex",
     justifyContent: "center",
     alignItems: "center",
-  },
-  button: {
-    fontSize: 30,
-    margin: 10,
-    padding: 10,
-    borderWidth: 2,
-    borderRadius: 10,
-    alignSelf: "stretch",
-    textAlign:"center",
-    padding: 5,
-    backgroundColor: "rgba(255,255,255, .8)"
-  },
-  buttonText: {
-    fontSize: 30,
-    textAlign: "center"
-  },
-  row: {
-    flexDirection: "row",
-    alignItems: "center"
-  },
-  sample: {
-    height:20,
-    width:20,
-    margin: 5,
-    borderRadius: 10,
-    backgroundColor: "white"
-  },
-  selectedText: {
-    backgroundColor: "red",
-    color: "yellow",
   }
 })
