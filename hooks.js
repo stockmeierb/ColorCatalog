4b62b959d2f09ed9b8c5eb215945ed4111a60204
brettstock <stockmeierb@users.noreply.github.com>
2024-08-13 16:24:39 -0500

    learning a whole lot about async react webhooks

--- hooks.js
+++ hooks.js
@@ -1,12 +1,35 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { generate } from "shortid";
+import {AsyncStorage} from "react-native";
 
 export const useColors = () => {
     const [colors, setColors] = useState([]);
+    
+    //Load colors from Async Storage
+    const loadColors = async () => {
+        const colorData = await AsyncStorage.getItem("@ColorListStore:Colors");
+        if (colorData) {
+            const colors = JSON.parse(colorData);
+            setColors(colors)
+        }
+    }
+
+
+    //Save Colors when colors array changes
+    useEffect(() => {
+        AsyncStorage.setItem("@ColorListStore:Colors", JSON.stringify(colors))
+    }, [colors])
+    
+    //Invokes Load Colors
+    useEffect(() => {
+        if (colors.length) return;
+        loadColors();
+    }, []);
+
     const addColor = color => {
       const newColor = {id: generate(), color};
       setColors([newColor, ...colors]);
     }
-    return { colors, addColor }
+    return { colors, addColor };
   }
   
