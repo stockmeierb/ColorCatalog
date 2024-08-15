e5bf72a93e8464e12306f3cf8e2229bd597ab805
brettstock <stockmeierb@users.noreply.github.com>
2024-08-15 14:03:25 -0500

    do not capitalize everything

--- components/ColorDetails.js
+++ components/ColorDetails.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
 
-export Default Function ColorDetails() {
+export default function ColorDetails() {
 return (
     <View style={styles.container}>
     <Text>Color Details</Text>
